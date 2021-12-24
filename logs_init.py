dcd77f42da5c03949838c187ddf8d97b71f3920a
NazarKarpiuk <nazar.karpiuk.ki.2018@lpnu.ua>
2021-12-24 02:13:28 +0200

    added pipepine, implement event logging system, refactored main.py, added requirements.txt

---
+++ logs_init.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging
+logger.basicConfig(
+    filename="proj_logs.log",
+    filemode="a",
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.INFO,
+)
